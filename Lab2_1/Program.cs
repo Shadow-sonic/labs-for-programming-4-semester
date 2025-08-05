9f387192c84c0853d972d1ebd88eaa15b26d2f04
sDenisss <ddenis22072006@gmail.com>
2025-08-05 21:00:56 +0600

    added lab2_1 and lab2_2

---
+++ Lab2_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введите что-нибудь: ");
+            string? input = Console.ReadLine();
+
+            if (input == null)
+            {
+                throw new Exception("вводные данные не могут быть пустыми");
+            }
+
+            int variant = ComputeVariant(input);
+            Console.WriteLine($"Вариант: {variant}");
+        }
+    }
+    
+    // Метод для вычисления варианта
+    public static int ComputeVariant(string input)
+    {
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+        foreach (char c in input)
+        {
+            int codePoint = (int)c; // Получаем Unicode-код символа
+            Console.WriteLine($"Код символа '{c}' : {codePoint}");
+            if (charCounts.ContainsKey(c))
+                charCounts[c]++;
+            else
+                charCounts[c] = 1;
+        }
+
+        long sum = 0;
+        foreach (var pair in charCounts)
+        {
+            sum += (long)Math.Pow((int)pair.Key, pair.Value);
+            System.Console.WriteLine(sum);
+        }
+
+        return (int)(sum % 8);
+    }
+
+}
